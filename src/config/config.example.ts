a54b3777b7cdc0844e2012f6eb8301abd63b434a
Alexander Memer <mkoaleksedos@gmail.com>
2019-06-06 14:56:33 +0300

    Обновление гитигнора, примерный конфиг

---
+++ src/config/config.example.ts
@@ -0,0 +1,19 @@
+export default {
+    jwtSecret: "",
+    mail: {
+        service: "gmail",
+        username: "",
+        password: ""
+    },
+    blockio: {
+        pin: "",
+        api_keys: {
+            "litecoin": "",
+            "bitcoin": "",
+            "dogecoin": "",
+            "litecoin_testnet": "",
+            "bitcoin_testnet": "",
+            "dogecoin_testnet": ""
+        }
+    }
+};

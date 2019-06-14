7a53ce0c6ea85d63ad49e5a8145868cbbc0ac28f
Alexander Memer <mkoaleksedos@gmail.com>
2019-06-14 13:37:33 +0300

    Заглушки пеера для модерации

--- src/controller/PayeerController.ts
+++ src/controller/PayeerController.ts
@@ -1,15 +1,40 @@
 import { Request, Response } from "express-serve-static-core";
 
 export default class PayeerController {
+
+    static ips = [
+        "185.71.65.92",
+        "185.71.65.189",
+        "149.202.17.210"
+    ];
+
     static status = async (req: Request, res: Response) => {
+        if (!PayeerController.ips.includes(req.ip)) {
+            return res.status(401).send();
+        }
+
         console.log(req.body);
+
+        return req.body["order_id"] + "|success";
     }
 
     static success = async (req: Request, res: Response) => {
+        if (!PayeerController.ips.includes(req.ip)) {
+            return res.status(401).send();
+        }
+
         console.log(req.body);
+
+        return req.body["order_id"] + "|success";
     }
 
     static fail = async (req: Request, res: Response) => {
+        if (!PayeerController.ips.includes(req.ip)) {
+            return res.status(401).send();
+        }
+
         console.log(req.body);
+
+        return req.body["order_id"] + "|success";
     }
 }

339cb4f09db8a1f468d7529600430047a302334b
Alexander Memer <mkoaleksedos@gmail.com>
2019-06-16 15:00:33 +0300

    История транзакций для Perfect Money

--- src/controller/PerfectMoneyController.ts
+++ src/controller/PerfectMoneyController.ts
@@ -5,17 +5,30 @@ import { getRepository } from "typeorm";
 import uuid = require("uuid/v4");
 
 import Deposit, { DepositStatus } from "../entity/Deposit";
+import Transaction, { TransactionStatus, TransactionType } from "../entity/Transaction";
 
 @Controller("api/payment/pm")
 export class PerfectMoneyController {
 
     @Post("status")
     private async status(req: Request, res: Response) {
+
+        let transaction = new Transaction();
+        transaction.amount_usd = parseFloat(req.body.PAYMENT_AMOUNT);
+        transaction.user_id = parseInt(req.body.USER_ID, 10);
+        transaction.status = TransactionStatus.DONE;
+        transaction.currency = "Perfect Money USD";
+        transaction.type = TransactionType.PAYIN;
+        transaction.dateCreated = new Date(moment().utc().format());
+        transaction.dateDone = new Date(moment().utc().format());
+
+        transaction = await getRepository(Transaction).save(transaction);
+
         let deposit = new Deposit();
-        deposit.user_id = parseInt(req.body.USER_ID, 10);
-        deposit.amount = parseFloat(req.body.PAYMENT_AMOUNT);
+        deposit.user_id = transaction.user_id;
+        deposit.amount = transaction.amount_usd;
         deposit.status = DepositStatus.PENDING;
-        deposit.transactionId = uuid();
+        deposit.transactionId = transaction.id;
         deposit.pendingEndTime = new Date(moment().utc().hours(20).minutes(59).add(48, "hours").format());
 
         deposit = await getRepository(Deposit).save(deposit);

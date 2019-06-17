9533ada0f0b1c337f141425adfec26c626bd5275
Alexander Memer <mkoaleksedos@gmail.com>
2019-06-17 10:22:03 +0300

    Включил конфиг с секретами

---
+++ src/config/config.ts
@@ -0,0 +1,31 @@
+export default {
+    jwtSecret: "bruh",
+    mail: {
+        service: "gmail",
+        username: "robofxtrading19@gmail.com",
+        password: "neqrod-kYjkux-jexro0"
+    },
+    blockio: {
+        pin: "",
+        api_keys: {
+            "litecoin": "",
+            "bitcoin": "",
+            "dogecoin": "",
+            "litecoin_testnet": "",
+            "bitcoin_testnet": "",
+            "dogecoin_testnet": ""
+        }
+    },
+    payeer: {
+       secret_key: "CZagstja",
+       additional_key: "f>V]&xR:k8>2",
+       account_id: "809708939"
+    },
+    latypay: {
+        secret_key: "7aQ]<\"Yck\"6a",
+        account_id: "72"
+    },
+    perfect_money: {
+        passphrase: "T8xc1Q4L9c4gQTFoowFHbmoHZ"
+    }
+};

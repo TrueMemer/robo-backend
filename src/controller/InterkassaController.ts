2188945e008bcd71247ad91c30c0362fab0a45dd
TrueMemer <mkoaleksedos@gmail.com>
2019-07-04 14:23:03 +0300

    Фикс JWT интеркассы

--- src/controller/InterkassaController.ts
+++ src/controller/InterkassaController.ts
@@ -3,7 +3,6 @@ import { Request, Response } from "express-serve-static-core";
 import { JWTChecker } from "../middlewares/JWTChecker";
 
 @Controller("api/payment/interkassa")
-@ClassMiddleware([JWTChecker])
 export class InterkassaController {
 
     @Post("interaction")

21c08f7282d760177dd42eac39d7b158350e61ac
TrueMemer <mkoaleksedos@gmail.com>
2019-07-11 09:29:46 +0300

    Обработчик payin-payout

--- src/controller/PIPOController.ts
+++ src/controller/PIPOController.ts
@@ -1,12 +1,14 @@
-import { Controller, ClassMiddleware, Post, Middleware } from "@overnightjs/core";
+import { Controller, Middleware, Post, Get } from "@overnightjs/core";
+import axios from "axios";
 import { Request, Response } from "express-serve-static-core";
-import { JWTChecker } from "../middlewares/JWTChecker";
-import * as uuid from "uuid/v4";
 import { getRepository } from "typeorm";
+import * as uuid from "uuid/v4";
 import User from "../entity/User";
+import { JWTChecker } from "../middlewares/JWTChecker";
 import moment = require("moment");
 import md5 = require("md5");
-import axios from "axios";
+import Transaction, { TransactionStatus } from "../entity/Transaction";
+import Deposit, { DepositStatus, DepositType } from "../entity/Deposit";
 
 @Controller("api/payment/pipo")
 export class PayinPayout {
@@ -34,7 +36,8 @@ export class PayinPayout {
             });
         }
 
-        const formAmount = ((amount * await r.data.ticker.price) + (amount * await r.data.ticker.price) * 0.055).toFixed(2);
+        const formAmount = ((amount * await r.data.ticker.price) +
+            (amount * await r.data.ticker.price) * 0.055).toFixed(2);
         console.log(formAmount);
 
         let sign_raw = `4283#${uid}#${agentTime}#${formAmount}#79090000001#${md5("W53prbl4uC")}`;
@@ -48,9 +51,9 @@ export class PayinPayout {
             "&amount=" + formAmount +
             "&goods=ROBO FX TRADING INVEST" +
             "&currency=USD" +
+            "&addInfo_userid=" + user.id +
             "&email=" + user.email +
             "&phone=79090000001" +
-            //"&preference=125" +
             "&agentTime=" + agentTime +
             "&sign=" + md5(sign_raw);
 
@@ -65,7 +68,34 @@ export class PayinPayout {
     @Post("status")
     private async status(req: Request, res: Response) {
 
-        console.log(req.body);
+        let t = await getRepository(Transaction).findOne(req.body.orderId);
+
+        if (!t) {
+            return res.status(200).send();
+        }
+
+        if (req.body.paymentStatus == 1) {
+            t.dateDone = new Date(Date.now());
+            t.status = TransactionStatus.DONE;
+
+            t = await getRepository(Transaction).save(t);
+
+            let d = new Deposit();
+
+            d.amount = t.amount_usd;
+            d.pendingEndTime = new Date(moment().utc().hours(20).minutes(59).add(48, "hours").format());
+            d.status = DepositStatus.PENDING;
+            d.transactionId = t.id;
+            d.type = DepositType.INVEST;
+            d.user_id = req.body.addInfo_userid;
+
+            d = await getRepository(Deposit).save(d);
+        } else if(req.body.paymentStatus == 2) {
+            t.dateDone = new Date(Date.now());
+            t.status = TransactionStatus.FAILED;
+
+            t = await getRepository(Transaction).save(t);
+        }
 
     }
 
